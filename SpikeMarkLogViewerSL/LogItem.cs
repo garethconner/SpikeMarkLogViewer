d0a870feb84dca2f46f5b5597518d9d81386f75f
unknown <Gareth@.(none)>
2010-08-05 00:33:30 -0400

    working silverlight implementation with simple log loading, but not filtering.

---
+++ SpikeMarkLogViewerSL/LogItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpikeMarkLogViewer
+{
+    public class LogItem
+    {
+        public LogItem(int Id, string Source, string Message, string MessageLevel, DateTime Timestamp)
+        {
+            this.Id = Id;
+            this.Source = Source;
+            this.Message = Message;
+            this.MessageLevel = MessageLevel;
+            this.TimeStamp = Timestamp;
+        }
+
+        public int Id { get; private set; }
+        public string Source { get; private set; }
+        public string Message { get; private set; }
+        public string MessageLevel { get; private set; }
+        public DateTime TimeStamp { get; private set; }
+
+    }
+}
---
+++ SpikeMarkLogViewerSL/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SpikeMarkLogViewer;
+
+namespace SpikeMarkLogViewerSL
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            grdLog.ItemsSource = Log.Instance.Items;
+        }
+
+        private void btnOpenLog_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Log Files (.log)|*.log|All Files (*.*)|*.*";
+            dlg.FilterIndex = 1;
+
+            bool? userClickedOK = dlg.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == true)
+            {
+                grdLog.ItemsSource = null;
+                // Open the selected file to read.
+                System.IO.Stream fileStream = dlg.File.OpenRead();
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    try
+                    {
+                        Log.Instance.InitializeFromStream(reader);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("There was an error reading this log file. { " + ex.StackTrace + " }");
+                    }
+                }
+                fileStream.Close();
+                grdLog.ItemsSource = Log.Instance.Items;
+            }
+        }
+    }
+}
